a2b6c4d4679067f674b1777c2a99efa2de3823fb
Israel Trejo <isrtrejo07@gmail.com>
2025-01-02 11:03:57 -0600

    Made the cart quantity to become interactive when we click on each item's  "Add to Cart" button

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -47,7 +47,7 @@ products.forEach((product) => {
           </div>
 
           <button class="add-to-cart-button button-primary js-add-to-cart"
-          data-product-name = "${product.name}">
+          data-product-id = "${product.id}">
             Add to Cart
           </button>
         </div>
@@ -58,12 +58,12 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
-    const productName = button.dataset.productName;
+    const productId = button.dataset.productId;
 
     let matchingItem;
 
     cart.forEach((item) => {
-      if(productName === item.productName){
+      if(productId === item.productId){
         matchingItem = item;
       }
     });
@@ -72,11 +72,20 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       matchingItem.quantity += 1;
     } else {
       cart.push({
-        productName: productName,
+        productId: productId,
         quantity: 1
       });
     }
 
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+    console.log(cartQuantity);
     console.log(cart);
   });
 });
