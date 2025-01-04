e1da9279b1560b899a1189f6d76c0f4032af4ddd
Israel Trejo <isrtrejo07@gmail.com>
2025-01-04 09:02:00 -0600

    Saving this code to add features like quantity saving from the "Add to Cart" page

--- data/cart.js
+++ data/cart.js
@@ -1,21 +1,84 @@
-// Normalizing the Data (deduplicating): removing dupilcate values from a data structure
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'));
+if (!cart) {
+  cart = [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2,
-}, {
+  }, {
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity: 1
-}];
-
-export function removeFromCart(productId){
-    const newCart = [];
-
-    // Purpose: Contain all the products that don't match the productId, which is the same thing as removing it from the checkout
-    cart.forEach((cartItem) => {
-        if(cartItem.productId != productId){
-            newCart.push(cartItem);
-        }
-    });
-
-    cart = newCart;
+  }];
 }
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+export function addToCart(productId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1
+    });
+  }
+
+  //     // Check if there's a previous timeout for this product. If there is, we should stop it
+//     const previousTimeoutId = addedMessageTimeouts[productId];
+//     if (previousTimeoutId) {
+//       clearTimeout(previousTimeoutId);
+//     }
+
+//     const timeoutId = setTimeout(() => {
+//       addedMessage.classList.remove('added-to-cart-visible');
+//     }, 2000);
+
+//     // Save the timeoutId for this product so we can stop it later if we need to
+//     addedMessageTimeouts[productId] = timeoutId;
+
+  saveToStorage();
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
+
+  saveToStorage();
+}
+
+// // Normalizing the Data (deduplicating): removing dupilcate values from a data structure
+// export let cart = [{
+//     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+//     quantity: 2,
+// }, {
+//     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+//     quantity: 1
+// }];
+
+// export function removeFromCart(productId){
+//     const newCart = [];
+
+//     // Purpose: Contain all the products that don't match the productId, which is the same thing as removing it from the checkout
+//     cart.forEach((cartItem) => {
+//         if(cartItem.productId != productId){
+//             newCart.push(cartItem);
+//         }
+//     });
+
+//     cart = newCart;
+// }
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,36 +1,32 @@
-// All imports needs to be at the top of the file
-// NEEDS to use Live Server
-// two dots (..) used to access a file RIGHT OUTSIDE the current file (amazon.js)
-import {cart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
-import { formatCurrency } from './utils/money.js';
+import {formatCurrency} from './utils/money.js';
 
 let productsHTML = '';
 
 products.forEach((product) => {
-    productsHTML += `
-        <div class="product-container">
-          <div class="product-image-container">
-            <img class="product-image"
-              src = "${product.image}">
-          </div>
+  productsHTML += `
+    <div class="product-container">
+      <div class="product-image-container">
+        <img class="product-image"
+          src="${product.image}">
+      </div>
 
-          <div class="product-name limit-text-to-2-lines">
-            ${product.name}
-          </div>
+      <div class="product-name limit-text-to-2-lines">
+        ${product.name}
+      </div>
 
-          <div class="product-rating-container">
-            <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.stars * 10}.png">
-            <div class="product-rating-count link-primary">
-              ${product.rating.count}
-            </div>
-          </div>
+      <div class="product-rating-container">
+        <img class="product-rating-stars"
+          src="images/ratings/rating-${product.rating.stars * 10}.png">
+        <div class="product-rating-count link-primary">
+          ${product.rating.count}
+        </div>
+      </div>
 
-          <div class="product-price">
-            $${formatCurrency(product.priceCents)}
-          </div>
-
+      <div class="product-price">
+        $${formatCurrency(product.priceCents)}
+      </div>
           <div class="product-quantity-container">
             <select class = "js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
@@ -45,83 +41,167 @@ products.forEach((product) => {
               <option value="10">10</option>
             </select>
           </div>
+      <div class="product-spacer"></div>
 
-          <div class="product-spacer"></div>
+      <div class="added-to-cart">
+        <img src="images/icons/checkmark.png">
+        Added
+      </div>
 
-          <div class="added-to-cart js-added-to-cart-${product.id}">
-            <img src="images/icons/checkmark.png">
-            Added
-          </div>
-
-          <button class="add-to-cart-button button-primary js-add-to-cart"
-          data-product-id = "${product.id}">
-            Add to Cart
-          </button>
-        </div>
-    `;
+      <button class="add-to-cart-button button-primary js-add-to-cart"
+      data-product-id="${product.id}">
+        Add to Cart
+      </button>
+    </div>
+  `;
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
-// We're going to use an object to save the timeout ids.
-// The reason we use an object is because each product will have its own timeoutId. So an object lets us save multiple timeout ids for different products
-const addedMessageTimeouts = {};
 
-// Copy + Paste into cart.js / export the function, then import into this page import {cart, addToCart} from '../data/cart.js';
-// function addToCart(productId)
-// function updateCartQuantity();
+function updateCartQuantity() {
+  let cartQuantity = 0;
 
-document.querySelectorAll('.js-add-to-cart').forEach((button) => {
-  button.addEventListener('click', () => {
-    const productId = button.dataset.productId;
-
-    let matchingItem;
-
-    cart.forEach((cartItem) => {
-      if(productId === cartItem.productId){
-        matchingItem = cartItem;
-      }
-    });
-
-    const quantitySelector = document.querySelector(
-      `.js-quantity-selector-${productId}`
-    );
-    const quantity = Number(quantitySelector.value);
-
-    if(matchingItem){
-      matchingItem.quantity += quantity;
-    } else {
-      cart.push({
-        productId: productId,
-        quantity: quantity
-      });
-    }
-
-    let cartQuantity = 0;
-
-    cart.forEach((cartItem) => {
-      cartQuantity += cartItem.quantity;
-    });
-
-    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-    const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
-    addedMessage.classList.add('added-to-cart-visible');
-    
-    // Check if there's a previous timeout for this product. If there is, we should stop it
-    const previousTimeoutId = addedMessageTimeouts[productId];
-    if (previousTimeoutId) {
-      clearTimeout(previousTimeoutId);
-    }
-
-    const timeoutId = setTimeout(() => {
-      addedMessage.classList.remove('added-to-cart-visible');
-    }, 2000);
-
-    // Save the timeoutId for this product so we can stop it later if we need to
-    addedMessageTimeouts[productId] = timeoutId;
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
   });
-});
 
-// Data Attribute: another HTML attribute, that allows us to attach any information to any element
-// - MUST start with "data-", then give it any name
+  document.querySelector('.js-cart-quantity')
+    .innerHTML = cartQuantity;
+}
 
-// Seperating words with dashes is called kebab-case
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
+      addToCart(productId);
+      updateCartQuantity();
+    });
+  });
+
+// // All imports needs to be at the top of the file
+// // NEEDS to use Live Server
+// // two dots (..) used to access a file RIGHT OUTSIDE the current file (amazon.js)
+// import {cart} from '../data/cart.js';
+// import {products} from '../data/products.js';
+// import { formatCurrency } from './utils/money.js';
+
+// let productsHTML = '';
+
+// products.forEach((product) => {
+//     productsHTML += `
+//         <div class="product-container">
+//           <div class="product-image-container">
+//             <img class="product-image"
+//               src = "${product.image}">
+//           </div>
+
+//           <div class="product-name limit-text-to-2-lines">
+//             ${product.name}
+//           </div>
+
+//           <div class="product-rating-container">
+//             <img class="product-rating-stars"
+//               src="images/ratings/rating-${product.rating.stars * 10}.png">
+//             <div class="product-rating-count link-primary">
+//               ${product.rating.count}
+//             </div>
+//           </div>
+
+//           <div class="product-price">
+//             $${formatCurrency(product.priceCents)}
+//           </div>
+
+//           <div class="product-quantity-container">
+//             <select class = "js-quantity-selector-${product.id}">
+//               <option selected value="1">1</option>
+//               <option value="2">2</option>
+//               <option value="3">3</option>
+//               <option value="4">4</option>
+//               <option value="5">5</option>
+//               <option value="6">6</option>
+//               <option value="7">7</option>
+//               <option value="8">8</option>
+//               <option value="9">9</option>
+//               <option value="10">10</option>
+//             </select>
+//           </div>
+
+//           <div class="product-spacer"></div>
+
+//           <div class="added-to-cart js-added-to-cart-${product.id}">
+//             <img src="images/icons/checkmark.png">
+//             Added
+//           </div>
+
+//           <button class="add-to-cart-button button-primary js-add-to-cart"
+//           data-product-id = "${product.id}">
+//             Add to Cart
+//           </button>
+//         </div>
+//     `;
+// });
+
+// document.querySelector('.js-products-grid').innerHTML = productsHTML;
+// // We're going to use an object to save the timeout ids.
+// // The reason we use an object is because each product will have its own timeoutId. So an object lets us save multiple timeout ids for different products
+// const addedMessageTimeouts = {};
+
+// // Copy + Paste into cart.js / export the function, then import into this page import {cart, addToCart} from '../data/cart.js';
+// // function addToCart(productId)
+// // function updateCartQuantity();
+
+// document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+//   button.addEventListener('click', () => {
+//     const productId = button.dataset.productId;
+
+//     let matchingItem;
+
+//     cart.forEach((cartItem) => {
+//       if(productId === cartItem.productId){
+//         matchingItem = cartItem;
+//       }
+//     });
+
+//     const quantitySelector = document.querySelector(
+//       `.js-quantity-selector-${productId}`
+//     );
+//     const quantity = Number(quantitySelector.value);
+
+//     if(matchingItem){
+//       matchingItem.quantity += quantity;
+//     } else {
+//       cart.push({
+//         productId: productId,
+//         quantity: quantity
+//       });
+//     }
+
+//     let cartQuantity = 0;
+
+//     cart.forEach((cartItem) => {
+//       cartQuantity += cartItem.quantity;
+//     });
+
+//     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+//     const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
+//     addedMessage.classList.add('added-to-cart-visible');
+    
+//     // Check if there's a previous timeout for this product. If there is, we should stop it
+//     const previousTimeoutId = addedMessageTimeouts[productId];
+//     if (previousTimeoutId) {
+//       clearTimeout(previousTimeoutId);
+//     }
+
+//     const timeoutId = setTimeout(() => {
+//       addedMessage.classList.remove('added-to-cart-visible');
+//     }, 2000);
+
+//     // Save the timeoutId for this product so we can stop it later if we need to
+//     addedMessageTimeouts[productId] = timeoutId;
+//   });
+// });
+
+// // Data Attribute: another HTML attribute, that allows us to attach any information to any element
+// // - MUST start with "data-", then give it any name
+
+// // Seperating words with dashes is called kebab-case
