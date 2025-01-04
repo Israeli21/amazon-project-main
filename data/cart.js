bc3d68583ab4542ec40e679f5f835e6c210bc227
Israel Trejo <isrtrejo07@gmail.com>
2025-01-04 13:17:26 -0600

    Successfully was able to intigrate all functions so far presented in the video tutorial by myself. Self-esteem has been boosted

--- data/cart.js
+++ data/cart.js
@@ -1,3 +1,4 @@
+// Normalizing the Data (deduplicating): removing dupilcate values from a data structure
 export let cart = JSON.parse(localStorage.getItem('cart'));
 if (!cart) {
   cart = [{
@@ -22,27 +23,19 @@ export function addToCart(productId) {
     }
   });
 
-  if (matchingItem) {
-    matchingItem.quantity += 1;
+  const quantitySelector = document.querySelector(
+    `.js-quantity-selector-${productId}`
+  );
+  const quantity = Number(quantitySelector.value);
+
+  if(matchingItem){
+    matchingItem.quantity += quantity;
   } else {
     cart.push({
       productId: productId,
-      quantity: 1
+      quantity: quantity
     });
   }
-
-  //     // Check if there's a previous timeout for this product. If there is, we should stop it
-//     const previousTimeoutId = addedMessageTimeouts[productId];
-//     if (previousTimeoutId) {
-//       clearTimeout(previousTimeoutId);
-//     }
-
-//     const timeoutId = setTimeout(() => {
-//       addedMessage.classList.remove('added-to-cart-visible');
-//     }, 2000);
-
-//     // Save the timeoutId for this product so we can stop it later if we need to
-//     addedMessageTimeouts[productId] = timeoutId;
 
   saveToStorage();
 }
@@ -50,6 +43,7 @@ export function addToCart(productId) {
 export function removeFromCart(productId) {
   const newCart = [];
 
+  // Purpose: Contain all the products that don't match the productId, which is the same thing as removing it from the checkout
   cart.forEach((cartItem) => {
     if (cartItem.productId !== productId) {
       newCart.push(cartItem);
@@ -60,25 +54,3 @@ export function removeFromCart(productId) {
 
   saveToStorage();
 }
-
-// // Normalizing the Data (deduplicating): removing dupilcate values from a data structure
-// export let cart = [{
-//     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-//     quantity: 2,
-// }, {
-//     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-//     quantity: 1
-// }];
-
-// export function removeFromCart(productId){
-//     const newCart = [];
-
-//     // Purpose: Contain all the products that don't match the productId, which is the same thing as removing it from the checkout
-//     cart.forEach((cartItem) => {
-//         if(cartItem.productId != productId){
-//             newCart.push(cartItem);
-//         }
-//     });
-
-//     cart = newCart;
-// }
