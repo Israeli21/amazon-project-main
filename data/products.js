627ed61a9edc3899e6ca59dc82cd45049e20a6e1
Israel Trejo <isrtrejo07@gmail.com>
2025-01-12 17:46:28 -0600

    Loaded products from the backend

--- data/products.js
+++ data/products.js
@@ -1,4 +1,4 @@
-// Stopped: 19:02:47
+// Stopped: 20:10:00
 import {formatCurrency} from '../scripts/utils/money.js';
 export function getProduct(productId){
   let matchingProduct;
@@ -66,6 +66,27 @@ class Clothing extends Product {
 // There's no object for 'this' to point to, so it will equal 'undefined'
 // console.log(this);
 
+export let products = [];
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    console.log('load products');
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -731,6 +752,7 @@ export const products = [
   }
   return new Product(productDetails);
 });
+*/
 
 // .map(): Loops through an array, for each value it runs a function
 // new Date(): generates an object that represents the current date
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,8 +2,11 @@
 // Saved data and generated HTML for payment summary
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
+import {loadProducts} from '../data/products.js';
 // import '../data/cart-class.js';
-import '../data/backend-practice.js';
+// import '../data/backend-practice.js';
 
-renderOrderSummary();
-renderPaymentSummary();
+loadProducts(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+});
